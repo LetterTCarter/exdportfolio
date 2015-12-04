0e3d65285b7b2fe9fb780952112284422eeac06a
T Jay Carter <tjaycarter@MacPro.local>
2015-12-03 22:21:45 -0700

    added slick, image fill, started styling
    Added jQuery plugins: Slick, ImageFill, ImagesLoaded. Started some CSS
    styling. Cleaned up the HTML. Need to learn to save and commit more
    often.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,18 @@ console.log('\'Allo \'Allo!');
 $(function() {
   console.log('Document is ready for action.');
 
-  //$('.week1').slick();
+  //hide the mainContent h1s
+  $('.mainContent h1').hide();
+
+  $('.projectImage').imagefill();
+  // image stretches to fill container
+
+  $('.week1, .week2, .week3, .week4, .week5, .week6, .week7, .week8').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
 
 });
